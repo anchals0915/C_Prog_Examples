6923d42714a729a470cd0eeb9c83e5efa0444729
anchals0915 <108451445+anchals0915@users.noreply.github.com>
2022-07-14 12:49:42 +0530

    Add files via upload

---
+++ Bubble_sorting.c
@@ -0,0 +1,47 @@
+//Sort the given set of n numbers using bubble sort technique
+//Ascending order
+#include<stdio.h>
+int main()
+{
+	int n;	///declaration of a variable
+	
+	//accept a size
+	printf("Enter a size of an array :");
+	scanf("%d", &n);
+	
+	//declaring an array 
+	int marks[n];
+	
+	//asking for input 
+	printf("Enter elements in an array:");
+	for(int i=0; i<n; i++)
+	{
+		scanf("%d" ,&marks[i]);
+	}
+	
+	//sorting array
+	for(int i=0; i<n-1 ; i++)
+	{
+		for(int j=0; j<n-1-i ; j++)
+		{
+			if(marks[j] > marks[j+1])
+			{
+				int temp = marks[j];
+				marks[j] = marks[j+1];
+				marks[j+1] = temp;
+			}
+		}
+	
+	}
+	
+	//printing array
+	printf("Sorted array :");
+	for(int i =0; i<n; i++)
+	{
+		printf("%d ", marks[i]);
+	}
+
+	return 0;
+}
+
+
